d7df29fa91090e238bb541ccc78dcb113215a0da
Digvijoy Mohapatra <digvijoymohapatra11@gmail.com>
2015-11-15 11:59:52 -0500

    initial commit

---
+++ us/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(us.Startup))]
+namespace us
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
